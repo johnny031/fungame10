e502112d01df6489c8530ff92b221dc16cf1615a
Johnny <johnny055238@gmail.com>
2021-09-10 11:23:22 +0800

    card color change

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ let current_player = -1
 let current_question = -1
 let schedule = 0
 let interval
+let colors = [["#f8b47c", "#d38443"], ["#e095c7", "#a77496"], ["#dbd24c", "#9e983c"], ["#06d4db", "#3093a0"]]
 
 for (let i = 0; i < names.length; i++) {
     $(".board").append(`
         <div>${names[i]}：<span id="${i}">0</span> 分</div>
     `)
 
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
 }
 
 function startTimer(duration) {
@@ -22,7 +31,7 @@ function startTimer(duration) {
             $("body").get(0).style.setProperty("--color2", "#a5890c")
         }
         if (timer < 167) {
-            $("body").get(0).style.setProperty("--color1", "#fd664b")
+            $("body").get(0).style.setProperty("--color1", "#f37058")
             $("body").get(0).style.setProperty("--color2", "#bb473f")
         }
         if (timer < 0) {
@@ -41,7 +50,12 @@ function next() {
     setTimeout(() => {
         current_question++
         $(".span").html(question[current_question])
+
     }, 500)
+
+    $("body").get(0).style.setProperty("--color3", colors[(current_question + 4) % 4][0])
+    $("body").get(0).style.setProperty("--color4", colors[(current_question + 4) % 4][1])
+
     if (current_player === names.length - 1) current_player = -1
 }
 
@@ -50,6 +64,7 @@ function add_score(x) {
     $(`#${id}`).html(parseInt($(`#${id}`).html()) + x)
 }
 
+shuffle(question)
 next()
 
 $("#card").flip({ trigger: "manual" })
@@ -71,14 +86,19 @@ $(".flip").on("click", function () {
         }
         $(this).html("下局")
     } else if (schedule % 4 === 3) {
-        $("body").get(0).style.setProperty("--color1", "#09d7e6")
-        $("body").get(0).style.setProperty("--color2", "#10a1b4")
+        $("body").get(0).style.setProperty("--color1", "#3ce093")
+        $("body").get(0).style.setProperty("--color2", "#13a360")
         $("#timer").text("5.00")
         $("#card").flip(false)
         $(this).html("翻牌")
         next()
     }
     schedule++
+})
+
+$(".back").on("dblclick", function () {
+    current_question++
+    $(".span").html(question[current_question])
 })
 
 $(".round").on("click", function () {
